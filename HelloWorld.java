9bae6b89f96f58b8998b09b7854d92568ecb20a2
masashi333 <masashi3331524@gmail.com>
2014-08-16 00:56:43 +0900

    HelloWorld.javaを追加

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+//変数とデータ型
+/*
+
+boolean true / false : boolean flag = true;
+char 1文字　'a'
+byte / short / int / long
+float double
+
+String 文字列　""
+*/
+
+public class HelloWorld{
+	public static void main(String[] args) {
+		int x = 5;
+
+		System.out.println(x);
+	}
+}
